0b0e4bbca30ad026e2ff21c20940147c3aa3515f
Mubeen Yasin <65570764+MubeenYasin@users.noreply.github.com>
2022-11-28 18:55:27 +0300

    Nave Error

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import Nave from '../../components/nave/Nave'
 import style from '../../styles/Header.module.css'
+import Nave from '../nave/Nave'
 
 
 const Header = () => {
     return (
         <div className='flex justify-center w-full h-52 bg-header-bg'>
             <Nave />
-
         </div>
     )
 }
