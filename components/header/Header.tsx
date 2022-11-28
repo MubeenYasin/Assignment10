8bd9d86aac8a3518c7ca92b7339cdf1105a43d8e
Mubeen Yasin <65570764+MubeenYasin@users.noreply.github.com>
2022-11-28 19:08:14 +0300

    nave deleted

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import style from '../../styles/Header.module.css'
-import Nave from '../nave/Nave'
 
 
 const Header = () => {
     return (
         <div className='flex justify-center w-full h-52 bg-header-bg'>
-            <Nave />
         </div>
     )
 }
