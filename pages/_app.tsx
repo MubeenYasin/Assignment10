3809a1c9e6bc6dff8d7e89599962816130fb24b5
Mubeen Yasin <65570764+MubeenYasin@users.noreply.github.com>
2022-11-28 22:46:56 +0300

    back to assignment10

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from '../components/header/Header'
 import Footer from '../components/footer/Footer'
-import Nave from '../components/nave/Nave'
+import Nave from '../components/nave/nave'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
