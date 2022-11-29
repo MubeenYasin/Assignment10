27ef622f06083cd919a36558fdbc1c44f7e389a7
Mubeen Yasin <65570764+MubeenYasin@users.noreply.github.com>
2022-11-29 22:04:16 +0300

    nave completed, Un-responsive

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Header from '../components/header/Header'
-import Footer from '../components/footer/Footer'
-import Nave from '../components/nave/nave'
+import Header from '../component/header/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
-    <div className='w-full'>
-      <Nave />
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </div>
+    <Header />
+    <Component {...pageProps} />
   </>
 }
